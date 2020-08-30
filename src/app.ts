201ef57850caa48fed787cc7953d0cdb403fea47
Taso Shyti <tasoshyti@live.com>
2020-08-30 15:22:44 +0200

    setup live reload

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
-import express from "express";
+import express, { Response as ExResponse, Request as ExRequest } from "express";
 import bodyParser from "body-parser";
+import swaggerUi from "swagger-ui-express";
 import { RegisterRoutes } from "../build/routes";
 
 const app = express();
@@ -12,5 +13,15 @@ app.use(
 
 app.use(bodyParser.json());
 RegisterRoutes(app);
+
+app.use(
+  "/swagger",
+  swaggerUi.serve,
+  async (_req: ExRequest, res: ExResponse) => {
+    return res.send(
+      swaggerUi.generateHTML(await import("../build/swagger.json"))
+    );
+  }
+);
 
 export default app;
