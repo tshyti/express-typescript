ca4937ac12e4176bf0de85b26b9d54a36f9da9cc
Taso Shyti <t.shyti@sitel.com.al>
2020-08-31 12:27:09 +0200

    simple cat schema

--- nestjs/src/app.module.ts
+++ nestjs/src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { CatsModule } from './cats/cats.module';
 import IndexController from './index.controller';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ envFilePath: '.development.env', isGlobal: true }),
+    MongooseModule.forRoot(process.env.DB_URL),
     CatsModule,
   ],
   controllers: [IndexController],
